02b8e309cc8b88536538431f17b453eacfeeaba2
Rafael Gonçalves <spdgrafael@gmail.com>
2023-09-06 15:26:02 +0100

    Commit 1 for restconsume app

---
+++ src/main/java/restcountries/com/restconsumeappcountries/RestconsumeAppCountriesApplication.java
@@ -0,0 +1,13 @@
+package restcountries.com.restconsumeappcountries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestconsumeAppCountriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestconsumeAppCountriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restcountries/com/restconsumeappcountries/RestconsumeAppCountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package restcountries.com.restconsumeappcountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestconsumeAppCountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
